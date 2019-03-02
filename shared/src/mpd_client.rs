d88f4a010a48f66f3645b61cc5d04c85b95f249a
Boris-Chengbiao Zhou <bobo1239@web.de>
2019-03-02 03:14:53 +0100

    WIP

--- shared/src/mpd_client.rs
+++ shared/src/mpd_client.rs
@@ -10,30 +10,35 @@ impl MpdClient {
         Ok(MpdClient(Client::connect(&address)?, address))
     }
 
-    fn do_op<T, F: Fn(&mut Client) -> Result<T, Error>>(&mut self, f: F) -> Result<T, Error> {
+    fn client(&mut self) -> &mut Client {
+        // TODO: Not sure why we're checking twice...
         if self.0.ping().is_err() || self.0.ping().is_err() {
             self.0 = Client::connect(&self.1).unwrap();
         }
-        f(&mut self.0)
+        &mut self.0
     }
 
     pub fn toggle_pause(&mut self) -> Result<(), Error> {
-        self.do_op(Client::toggle_pause)
+        self.client().toggle_pause()
     }
 
     pub fn next(&mut self) -> Result<(), Error> {
-        self.do_op(Client::next)
+        self.client().next()
     }
 
     pub fn prev(&mut self) -> Result<(), Error> {
-        self.do_op(Client::prev)
+        self.client().prev()
+    }
+
+    pub fn switch(&mut self, queue_place: u32) -> Result<(), Error> {
+        self.client().switch(queue_place)
     }
 
     pub fn current_song(&mut self) -> Result<Option<Song>, Error> {
-        self.do_op(Client::currentsong)
+        self.client().currentsong()
     }
 
     pub fn queue(&mut self) -> Result<Vec<Song>, Error> {
-        self.do_op(Client::queue)
+        self.client().queue()
     }
 }
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,13 +1,10 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
-
 use std::sync::Mutex;
 
 use askama::Template;
 use itertools::izip;
-use rocket::State;
+use rocket::{get, routes, State};
 use rocket_contrib::serve::StaticFiles;
 
 use shared::config::Config;
@@ -18,34 +15,12 @@ pub fn launch(config: &Config) {
     rocket::ignite()
         .manage(Mutex::new(MpdClient::connect(config.mpd_address).unwrap()))
         .manage(Romanizer::new().unwrap())
-        .mount("/", routes![index, next, test])
-        .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
+        .mount("/", routes![index, next, switch])
+        .mount(
+            "/",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")),
+        )
         .launch();
-}
-
-#[get("/")]
-fn index(mpd: State<Mutex<MpdClient>>, romanizer: State<Romanizer>) -> String {
-    let songs = mpd.lock().unwrap().queue().unwrap();
-
-    let titles: Vec<_> = songs
-        .iter()
-        .map(|s| {
-            s.title
-                .as_ref()
-                .cloned()
-                .unwrap_or_else(|| "[missing title]".to_string())
-        })
-        .collect();
-    let romanized_titles = romanizer.romanize(&titles.join("\n"));
-    let romanized_titles: Vec<_> = romanized_titles.split('\n').collect();
-
-    assert_eq!(songs.len(), titles.len());
-    assert_eq!(songs.len(), romanized_titles.len());
-    let mut output = String::new();
-    for (song, title, romanized) in izip!(songs, titles, romanized_titles) {
-        output += &format!("{} {}   |  {}\n", song.place.unwrap().id, title, romanized,);
-    }
-    output
 }
 
 #[get("/next")]
@@ -55,13 +30,83 @@ fn next(mpd: State<Mutex<MpdClient>>) -> &str {
     "Skipped"
 }
 
-#[get("/play")]
-fn test() -> PlayTemplate<'static> {
-    PlayTemplate { name: "testing" }
+#[get("/switch/<place>")]
+fn switch(mpd: State<Mutex<MpdClient>>, place: u32) -> &str {
+    let mut mpd = mpd.lock().unwrap();
+    mpd.switch(place).unwrap();
+    "Switched"
+}
+
+#[get("/")]
+fn index(mpd: State<Mutex<MpdClient>>, romanizer: State<Romanizer>) -> IndexTemplate {
+    let songs = mpd.lock().unwrap().queue().unwrap();
+
+    // TODO: Cache romanization somehow...
+    let titles: Vec<_> = songs
+        .iter()
+        .map(|s| {
+            s.title
+                .as_ref()
+                .cloned()
+                .unwrap_or_else(|| "[missing title]".to_string())
+        })
+        .collect();
+    let titles_romanized: Vec<_> = romanizer
+        .romanize(&titles.join("\n"))
+        .split('\n')
+        .map(|s| s.to_string())
+        .collect();
+
+    assert_eq!(songs.len(), titles.len());
+    assert_eq!(songs.len(), titles_romanized.len());
+
+    // TODO: Use a map or something... (also see TODO above)
+    let albums: Vec<_> = songs
+        .iter()
+        .map(|s| {
+            s.tags
+                .get("Album")
+                .cloned()
+                .unwrap_or_else(|| "[missing album]".to_string())
+        })
+        .collect();
+    let albums_romanized: Vec<_> = romanizer
+        .romanize(&albums.join("\n"))
+        .split('\n')
+        .map(|s| s.to_string())
+        .collect();
+
+    assert_eq!(songs.len(), albums.len());
+    assert_eq!(songs.len(), albums_romanized.len());
+
+    let song_infos: Vec<SongInfo> =
+        izip!(songs, titles, titles_romanized, albums, albums_romanized)
+            .map(
+                |(song, title, title_romanized, album, album_romanized)| SongInfo {
+                    place: song.place.unwrap().id.0 - 1,
+                    title,
+                    title_romanized,
+                    album,
+                    album_romanized,
+                },
+            )
+            .collect();
+
+    IndexTemplate {
+        song_infos: song_infos,
+    }
 }
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct PlayTemplate<'a> {
-    name: &'a str,
+struct IndexTemplate {
+    song_infos: Vec<SongInfo>,
+}
+
+struct SongInfo {
+    place: u32,
+    title: String,
+    title_romanized: String,
+    album: String,
+    album_romanized: String,
 }
